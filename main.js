dd18e43179bd332eee348e298a9a92e3e2a2b536
Emerson Fitzgerald <10836343@my.uvu.edu>
2020-04-21 15:26:39 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+
+document.querySelector("#websiteName").textContent = "Hotel Finder"
+
+document.querySelector("#slogan").textContent = "Find your perfect hotel!"
+
+
+
+async function getHotelData(){
+    try {
+        const response = await fetch('hotel.json')
+        return await response.json() //returns JSON object
+
+    } catch(error) {
+        console.error(error)
+    }
+}
+let hotelData = {}
+getHotelData().then(data => (hotelData = data))
+
+//store in a variable document.querySelectorAll("a")
+//use that variable to loop over each element and addEventListener to each one
+//use a forEach loop
+//let buttons = document.querySelectorAll("a")
+
+
+
+
+
+document.querySelector("#mariott").addEventListener('click', hotelInfo)
+document.querySelector("#sheraton").addEventListener('click', hotelInfo)
+document.querySelector("#hilton").addEventListener('click', hotelInfo)
+
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    }) 
+   
+    console.log(hotelChoice)
+document.querySelector("#hotelName").textContent = `${hotelChoice.name} Hotel`
+document.querySelector('#address').textContent = `${hotelChoice.address}`
+document.querySelector('#rooms').textContent = ` ${hotelChoice.rooms}`
+document.querySelector('#gym').textContent = ` ${hotelChoice.gym}`
+document.querySelector('#type').textContent = ` ${hotelChoice.roomType}`
+document.querySelector('#picture').src = `${hotelChoice.picture}`
+}
